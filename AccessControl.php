942c364c26332491efa20855e2030a386c9195d8
Kghbln <karsten@hoffmeyer.info>
2016-06-03 17:46:25 +0200

    Add license information and other small tweaks
    * Provide COPYING file containing license information per
    https://www.mediawiki.org/wiki/Project:WikiProject_Extensions/Projects/Recovery/Candidate1
    * Add license label to show on "Special:Version"
    * Improve and extend file documentation
    * Go https for links
    * Add info about version 2.6 to changelog
    
    Bug: T123943
    Change-Id: Ibc2dcfaeb8810ed1e19c5eb65cfd705907bc180e

--- AccessControl.php
+++ AccessControl.php
@@ -1,42 +1,56 @@
 <?php
 
-/* MediaWiki extension that enables group access restriction on a page-by-page
- * basis contributed by Martin Mueller (http://blog.pagansoft.de) based into
- * version 1.3 on accesscontrol.php by Josh Greenberg.
- * Version 2.0 for MediaWiki >= 1.18 rewrited completly by Aleš Kapica.
+/**
+ * MediaWiki extension that enables group access restriction on a page-by-page
+ * basis
+ *
+ * Version pre-0.1 (2005-05-03) by Josh Greenberg
+ * Version 0.1 to 0.9 (2010-06-27) by Martin Gondermann
+ * Version 1.0 to 2.5 by Aleš Kapica
+ * Version 2.6 by Siebrand Mazeland and Thomas Mulhall
+ *
  * @package MediaWiki
  * @subpackage Extensions
  * @author Aleš Kapica
  * @copyright 2008-2014 Aleš Kapica
- * @licence GNU General Public Licence
+ * @license GNU General Public Licence 2.0 or later
  */
 
+// Ensure that the script cannot be executed outside of MediaWiki
 if ( !defined( 'MEDIAWIKI' ) ) {
 	echo "This file is an extension to the MediaWiki software and cannot be used standalone.\n";
 	die();
 }
 
-// sysop users can read all restricted pages
-$wgAdminCanReadAll = true;
-$wgAccessControlRedirect = true;
-
+// Register extension with MediaWiki
 $wgExtensionCredits['parserhook'][] = [
 	'path' => __FILE__,
 	'name' => 'AccessControl',
-	'author' => [ '[https://www.mediawiki.org/wiki/m:User:Want Aleš Kapica]' ],
-	'url' => 'http://www.mediawiki.org/wiki/Extension:AccessControl',
+	'author' => [
+		'[https://www.mediawiki.org/wiki/m:User:Want Aleš Kapica]'
+	],
+	'url' => 'https://www.mediawiki.org/wiki/Extension:AccessControl',
 	'version' => '2.6',
-	'descriptionmsg' => 'accesscontrol-desc'
+	'descriptionmsg' => 'accesscontrol-desc',
+	'license-name' => 'GPL-2.0+'
 ];
 
+// Set extension specific parameters
+// sysop users can read all restricted pages
+$wgAdminCanReadAll = true;
+// do not redirect from page in search results to restricted pages
+$wgAccessControlRedirect = true;
+
+// Load extension's class
 $wgAutoloadClasses['AccessControlHooks'] = __DIR__ . '/AccessControl.hooks.php';
 
-$wgHooks['ParserFirstCallInit'][] = 'AccessControlHooks::accessControlExtension';
-
+// Register extension's messages
 $wgMessagesDirs['AccessControl'] = __DIR__ . '/i18n';
 
+// Register hooks
+// Hook the ParserFirstCallInit for
+$wgHooks['ParserFirstCallInit'][] = 'AccessControlHooks::accessControlExtension';
 // Hook the userCan function for bypassing the cache
 $wgHooks['userCan'][] = 'AccessControlHooks::onUserCan';
-
 // Hook the UnknownAction function for information user about restrictions
 $wgHooks['UnknownAction'][] = 'AccessControlHooks::onUnknownAction';
